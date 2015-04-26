6425d9675f619b214517be2004e03d9c31901ccc
Rob Wise <robert.wise@outlook.com>
2015-04-25 22:47:13 -0400

    add package-info.java for output package

---
+++ src/amazon_product_metadata_parser/output/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A collection of classes used by instances of {@link amazon_product_metadata_parser.Parser} to
+ * send parsed {@link amazon_product_metadata_parser.dto.ProductDTO} objects. This allows for
+ * reusing the same {@code Parser} class while changing how the parsed data is used.
+ */
+package amazon_product_metadata_parser.output;
