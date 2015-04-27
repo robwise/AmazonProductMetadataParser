b71ec15ea53cefe16c6b8cf193f4dec95e3ca202
Rob Wise <robert.wise@outlook.com>
2015-04-27 00:03:33 -0400

    add missing package-info.java files

---
+++ src/amazon_product_metadata_parser/output/SQLServer2012/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes relating to outputting product data to a SQLServer2012 database.
+ */
+package amazon_product_metadata_parser.output.SQLServer2012;
---
+++ src/amazon_product_metadata_parser/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Library for parsing an 'amazon-meta.txt' file into Java objects to be output in a customizable
+ * way.
+ */
+package amazon_product_metadata_parser;
