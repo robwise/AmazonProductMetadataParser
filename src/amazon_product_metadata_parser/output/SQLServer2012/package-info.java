89a8be87415ec3afb938322555499c369619b549
Rob Wise <robert.wise@outlook.com>
2015-04-29 21:11:37 -0400

    add package-info.java to SQLServer2012 package

---
+++ src/amazon_product_metadata_parser/output/SQLServer2012/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes relating to a type of output {@link amazon_product_metadata_parser.output.Output} that
+ * connects to a SQL Server 2012 database and executes JDBC statements supplied with {@link
+ * amazon_product_metadata_parser.dto.ProductDTO}s.
+ */
+package amazon_product_metadata_parser.output.SQLServer2012;
